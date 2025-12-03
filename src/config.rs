fc9383f76e5d2e3f09e29145bfde3e139e9d8d70
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-12-03 19:20:16 -0300

    fix: expand ~/ path on config file

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,9 @@
 use {
-    crate::error::ScillaError,
+    crate::{constants::SCILLA_CONFIG_RELATIVE_PATH, error::ScillaError},
     serde::{Deserialize, Serialize},
     solana_commitment_config::CommitmentLevel,
     std::{env::home_dir, fs, path::PathBuf},
 };
-
-pub const SCILLA_CONFIG_RELATIVE_PATH: &str = ".config/scilla.toml";
 
 pub fn scilla_config_path() -> PathBuf {
     let mut path = home_dir().expect("Error getting home path");
@@ -13,18 +11,36 @@ pub fn scilla_config_path() -> PathBuf {
     path
 }
 
+pub fn expand_tilde(path: &str) -> PathBuf {
+    // on tomls, the ~ is not automatically expanded, so we need to do it manually
+    if path.starts_with("~/")
+        && let Some(home) = home_dir() {
+            return path.replacen("~", &home.to_string_lossy(), 1).into();
+        }
+    path.into()
+}
+
+fn deserialize_path_with_tilde<'de, D>(deserializer: D) -> Result<PathBuf, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s: String = Deserialize::deserialize(deserializer)?;
+    Ok(expand_tilde(&s))
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct ScillaConfig {
     pub rpc_url: String,
     pub commitment_level: CommitmentLevel,
+    #[serde(deserialize_with = "deserialize_path_with_tilde")]
     pub keypair_path: PathBuf,
 }
 
 impl ScillaConfig {
     pub fn load() -> Result<ScillaConfig, ScillaError> {
         let scilla_config_path = scilla_config_path();
-        println!("{:?}", scilla_config_path);
+        println!("Config Path Founded! Using {scilla_config_path:?}");
         if !scilla_config_path.exists() {
             return Err(ScillaError::ConfigPathDoesntExists);
         }
